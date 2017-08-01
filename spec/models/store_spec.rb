10456893a8bb0ed0b9a96893d6986c738c181a80
Zhumaniyazov Kairat <k.zhumaniyazov@gmail.com>
2017-08-01 23:09:45 +0600

    + stores list with publisher books

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -18,32 +18,7 @@ RSpec.describe Store, type: :model do
 
   describe 'scopes' do
     context 'with available books' do
-      let(:publisher) { create :publisher }
-      let(:another_publisher) { create :publisher }
-
-      let(:book_1) { create :book, publisher: publisher }
-      let(:book_2) { create :book, publisher: publisher }
-      let(:book_3) { create :book, publisher: another_publisher }
-
-      let(:store_1) { create :store }
-      let(:store_2) { create :store }
-      let(:store_3) { create :store }
-
-      subject { Store.with_available_books_for_publisher(publisher) }
-
-      before do
-        Stock.create store: store_1, book: book_1, amount: 2
-        Stock.create store: store_2, book: book_1, amount: 0
-        Stock.create store: store_3, book: book_1, amount: 0
-
-        Stock.create store: store_1, book: book_2, amount: 0
-        Stock.create store: store_2, book: book_2, amount: 1
-        Stock.create store: store_3, book: book_2, amount: 0
-
-        Stock.create store: store_1, book: book_3, amount: 0
-        Stock.create store: store_2, book: book_3, amount: 1
-        Stock.create store: store_3, book: book_3, amount: 2
-      end
+      include_context 'books_and_stores'
 
       it { expect(subject.count).to eq 2 }
       it { expect(subject).to include(store_1) }
