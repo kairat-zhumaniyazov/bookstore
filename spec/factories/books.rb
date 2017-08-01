a005f50fabc8c5eabeb780e28b45af2407483f90
Zhumaniyazov Kairat <k.zhumaniyazov@gmail.com>
2017-08-01 16:53:50 +0600

    + Book model

---
+++ spec/factories/books.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryGirl.define do
+  factory :book do
+    
+  end
+end
---
+++ spec/models/book_spec.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  it { should validate_presence_of :title }
+end
